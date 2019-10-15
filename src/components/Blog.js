f7a3e02438cad99d250baa8c4046b021986ec854
Janne Peippo <peippo@lumous.com>
2019-10-15 21:44:22 +0300

    Add osa 5a 5.6 answer

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Blog = ({ blog }) => (
-	<div>
-		{blog.title} {blog.author}
-	</div>
-);
+const Blog = ({ blog }) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	let addedBy = null;
+	if (blog.user) {
+		addedBy = <p>Added by {blog.user.name}</p>;
+	}
+
+	if (!isOpen) {
+		return (
+			<div
+				onClick={() => setIsOpen(!isOpen)}
+				style={{
+					border: "1px solid gray",
+					padding: "10px",
+					marginTop: "5px"
+				}}
+			>
+				<b>{blog.title}</b> by {blog.author}
+			</div>
+		);
+	} else {
+		return (
+			<div
+				style={{
+					position: "relative",
+					border: "1px solid gray",
+					padding: "10px",
+					marginTop: "5px"
+				}}
+			>
+				<div
+					onClick={() => setIsOpen(!isOpen)}
+					style={{
+						background: "black",
+						color: "white",
+						margin: "-10px -10px 10px -10px",
+						padding: "10px"
+					}}
+				>
+					<b>{blog.title}</b> by {blog.author}
+				</div>
+				<p>
+					<a href={blog.url}>{blog.url}</a>
+				</p>
+				<p>
+					{blog.likes} likes <button>+1 like</button>
+				</p>
+				{addedBy}
+			</div>
+		);
+	}
+};
 
 export default Blog;
